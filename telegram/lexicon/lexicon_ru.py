9414c164befbe8c09fc2465383af249675ef4957
4erdenko <4erdenko@tuta.io>
2023-10-31 15:18:17 +0300

    add bot author

--- telegram/lexicon/lexicon_ru.py
+++ telegram/lexicon/lexicon_ru.py
@@ -2,7 +2,8 @@ LEXICON_RU: dict[str, str] = {
     '/start': 'Привет, это бот для тестового задания по вот этой вот '
     '<a '
     'href="https://docs.google.com/document/d/1XRR5m0PMocZf'
-    '-AwM1BFNYUJ2OdBEkm9YYH8uQjYlvQc/edit">ссылочке</a>',
+    '-AwM1BFNYUJ2OdBEkm9YYH8uQjYlvQc/edit">ссылочке</a>.\n'
+    'Автор бота: https://t.me/Landmesser_A',
     'other_answer': 'Невалидный запрос\\. Пример запроса:\n'
     '```json\n\\{\\"dt\\_from\\": \\"2022\\-09\\-01T00:00:00\\", \\"dt\\_upto\\": '
     '\\"2022\\-12\\-31T23:59:00\\", \\"group\\_type\\": \\"month\\"\\}```\n'
