3d40032352270d59ad9d515e2f076301f674f5ba
4erdenko <4erdenko@tuta.io>
2023-10-31 18:36:37 +0300

    added types, refactoring

--- database/db.py
+++ database/db.py
@@ -1,15 +1,19 @@
-import motor.motor_asyncio
+from typing import List
+
+from motor.motor_asyncio import (AsyncIOMotorClient, AsyncIOMotorCollection,
+                                 AsyncIOMotorDatabase)
 
 from config import COLLECTION, DATABASE, MONGO_HOST, MONGO_PORT
 
 
 class MongoBase:
-    def __init__(self):
-        connection_string = f'mongodb://{MONGO_HOST}:{MONGO_PORT}/{DATABASE}'
-        self.client = motor.motor_asyncio.AsyncIOMotorClient(connection_string)
-        self.db = self.client[DATABASE]
-        self.collection = self.db[COLLECTION]
+    def __init__(self) -> None:
+        connection_string: str = (
+            f'mongodb://{MONGO_HOST}:{MONGO_PORT}/{DATABASE}'
+        )
+        self.client: AsyncIOMotorClient = AsyncIOMotorClient(connection_string)
+        self.db: AsyncIOMotorDatabase = self.client[DATABASE]
+        self.collection: AsyncIOMotorCollection = self.db[COLLECTION]
 
-    async def aggregate(self, pipeline: list):
-        cursor = self.collection.aggregate(pipeline)
-        return await cursor.to_list(length=None)
+    async def aggregate(self, pipeline: List[dict]) -> List[dict]:
+        return await self.collection.aggregate(pipeline).to_list(length=None)
