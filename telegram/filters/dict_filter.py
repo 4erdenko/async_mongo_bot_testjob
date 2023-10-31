58b4bc9375217ab7c319e1de82032538801f5b99
4erdenko <4erdenko@tuta.io>
2023-10-31 18:37:12 +0300

    added types, instance checker, refactoring

--- telegram/filters/dict_filter.py
+++ telegram/filters/dict_filter.py
@@ -5,9 +5,11 @@ from aiogram.types import Message
 
 
 class IsJSONWithFields(Filter):
-    async def __call__(self, message: Message):
+    async def __call__(self, message: Message) -> bool:
         try:
             data = json.loads(message.text)
+            if not isinstance(data, dict):
+                return False
             return all(
                 field in data for field in ['dt_from', 'dt_upto', 'group_type']
             )
